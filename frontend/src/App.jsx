a552b2ac1cbc5008977cf8b57cc0e628c60bcd63
Balaji <balajireddy9976@gmail.com>
2026-06-09 15:13:54 +0530

    fix: add top padding to user home page to prevent navbar overlap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -95,7 +95,7 @@ function AppContent() {
       {!(location.pathname === '/' && !isLoggedIn) && location.pathname !== '/signin' && location.pathname !== '/signup' && <Navbar />}
 
       <main
-        className={`flex-grow flex flex-col ${location.pathname.startsWith('/solve') || location.pathname.startsWith('/challenge') || location.pathname.startsWith('/course-challenge') || location.pathname.startsWith('/course-project') || location.pathname.startsWith('/courses') ? 'pt-0 lg:pt-16' : (location.pathname === '/' ? 'pt-0' : 'pt-14 lg:pt-16')} pb-24 sm:pb-0 max-w-[100vw] overflow-x-hidden`}
+        className={`flex-grow flex flex-col ${location.pathname.startsWith('/solve') || location.pathname.startsWith('/challenge') || location.pathname.startsWith('/course-challenge') || location.pathname.startsWith('/course-project') || location.pathname.startsWith('/courses') ? 'pt-0 lg:pt-16' : (location.pathname === '/' && !isLoggedIn ? 'pt-0' : 'pt-14 lg:pt-16')} pb-24 sm:pb-0 max-w-[100vw] overflow-x-hidden`}
         style={{ minHeight: '60vh' }}
       >
 
